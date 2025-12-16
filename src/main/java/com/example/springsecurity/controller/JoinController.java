6c3e2737846f71c3d8c44da148c96736bfa7ce02
Bogeun Kim <mmmjjjkim@naver.com>
2025-12-16 21:03:15 +0900

    refactor: 주석 제거

--- src/main/java/com/example/springsecurity/controller/JoinController.java
+++ src/main/java/com/example/springsecurity/controller/JoinController.java
@@ -21,15 +21,6 @@ public class JoinController {
 
     @PostMapping("/joinProc")
     public String joinProcess(JoinDTO joinDTO) {
-        /*
-        * 회원가입이 완료되면 로그인 페이지로 옮기고
-        * 회원가입이 실패하면 다시 회원가입 페이지로 돌아가도록 설정해야하는데
-        * 가장 기본적인 작업만 할 예정이기 때문에 바로 로그인 페이지로 리다이렉팅함.
-        * */
-
-        // form 데이터 뽑아내보기
-//        System.out.println(joinDTO.getUsername());
-//        System.out.println(joinDTO.getPassword());
 
         joinService.joinProcess(joinDTO);
 

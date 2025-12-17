32cf27845e04bc0b7339a8a9a7a131013c7b8cb8
Bogeun Kim <mmmjjjkim@naver.com>
2025-12-17 18:20:45 +0900

    feat: 회원 중복 검증 메서드 추가

--- src/main/java/com/example/springsecurity/repository/UserRepository.java
+++ src/main/java/com/example/springsecurity/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.springsecurity.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    boolean existsByUsername(String username);
 }
